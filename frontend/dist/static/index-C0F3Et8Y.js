831bfc2ad19f3e69566beb2a254932cbd4543000
wujianing <729518161@qq.com>
2025-09-07 13:26:19 +0800

    feat:dist

---
+++ frontend/dist/static/index-C0F3Et8Y.js
@@ -0,0 +1,1 @@
+import{p as t,aD as r,aC as a,q as o,t as s}from"./vue-DJH8E3j2.js";const m=t({__name:"index",setup(n){const e=a();return r().replace({path:`/${e.params.path}`,query:e.query}),(p,c)=>(o(),s("div"))}});export{m as default};
